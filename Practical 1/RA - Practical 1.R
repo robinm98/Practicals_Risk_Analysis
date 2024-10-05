0a4fee6c76075c5fc0dead4766f81007cd539f84
robinm98 <robin.michel@unil.ch>
2024-10-05 16:48:24 +0200

    Relative path + Begining of Part 2

--- Practical 1/RA - Practical 1.R
+++ Practical 1/RA - Practical 1.R
@@ -6,13 +6,13 @@ library(readr)
 library(tseries)
 library(nortest)
 library(MASS)
+library(fpp2)
 
 
-# Set wd
-setwd("/Users/robin/Library/Mobile Documents/com~apple~CloudDocs/UNIL/HEC cours/Master/MA3/Risk Analytics/Practicals/Practical 1")
-
-# Load data
+# Load data using a relative path
 Crypto_data <- read_csv("Data/Crypto_data.csv")
+
+# View the data
 View(Crypto_data)
 
 ################## PART 1 ##################
@@ -165,12 +165,85 @@ than if it were normally distributed."
 
 ############################################################################################################################################
 
-### Part 2 ###
+################## PART 2 ##################
+
+### a
 
 # Plot the ACF of the raw Bitcoin series
-ggAcf(bitcoin_prices, main = "ACF of Negative Log Returns")
+ggAcf(bitcoin_prices, main = "ACF of Negative Log Returns") # significant autocorrelation, non-stationary, difficult to model
 
 # Plot the ACF of the negative log returns
-ggAcf(bitcoin_negative_log_returns, main = "ACF of Negative Log Returns")
+ggAcf(bitcoin_negative_log_returns, main = "ACF of Negative Log Returns") # no significant autocorrelation, stationary, easier to model
+
+"Non-stationary time series are more difficult to model because the key statistical properties like mean, variance, 
+and autocovariance change over time. This variability makes it challenging to build predictive models since the underlying 
+structure of the data is constantly shifting." 
+
+"Stationary time series are easier to model because their statistical properties are constant over time. This stability 
+allows models (like ARMA, ARIMA, or GARCH) to assume that past behavior can be used to predict future behavior, given that 
+the underlying dynamics of the series don't change."
+
+### b
+
+# Ljung-Box test : H0 = no autocorrelation (white noise) / H1 = autocorrelation in the data
+# --> a ts with some autocorrelation can still be stationary
+
+# Perform Ljung-Box test for the raw Bitcoin series
+ljung_box_raw <- Box.test(bitcoin_prices, lag = 20, type = "Ljung-Box") # lag = 20 because rule of thumb n/10
+
+# Print the results for the raw series
+print(ljung_box_raw) # --> p-value < 0.05, so we reject H0, there is autocorrelation in the data
+
+# Perform Ljung-Box test for the negative log returns
+ljung_box_neg_log <- Box.test(bitcoin_negative_log_returns, lag = 20, type = "Ljung-Box")
+
+# Print the results for the negative log returns
+print(ljung_box_neg_log) # --> p-value < 0.05, so we reject H0, there is autocorrelation in the data BUT doesn't mean non-stationary
+
+"Even though the negative log returns are stationary, they still exhibit some autocorrelation but the autocorellation 
+is much lower for the negative log returns compared to the raw Bitcoin series."
+
+### c
+
+# Plot the ACF of the negative log returns
+ggAcf(bitcoin_negative_log_returns, main = "ACF of Negative Log Returns") # Spike at lag 2 so p=2
+
+# Plot the PACF of the negative log returns
+ggPacf(bitcoin_negative_log_returns, main = "PACF of Negative Log Returns") # Spike at lag 2 so p=2
+
+# Fit an ARIMA(2, 0,2) model to the negative log returns (d=0 because non-stationarity form the ACF and PACF)
+arima_fit <- arima(bitcoin_negative_log_returns, order = c(2,0,2))
+print(arima_fit)
+
+# Fit an auto.arima model to the negative log returns
+auto_arima_fit <- auto.arima(bitcoin_negative_log_returns)
+print(auto_arima_fit) # Auto.arima() suggests also an ARIMA(2,0,2) model, so it confirms the choice of the ARIMA model.
+
+### Asses residuals of the ARIMA model
+
+# Extract the residuals
+residuals_arima <- residuals(arima_fit)
+
+# ACF of residuals (to check for autocorrelation)
+acf(residuals_arima, main = "ACF of ARIMA Model Residuals")
+
+# Ljung-Box test on residuals (to formally test for autocorrelation)
+Box.test(residuals_arima, lag = 20, type = "Ljung-Box") # There is no significant autocorrelation in the residuals of the ARIMA model, which means the model has fully captured the patterns in the data (good sign)
+
+# QQ-plot of residuals (to check for normality)
+qqnorm(residuals_arima)
+qqline(residuals_arima, col = "red") # residuals don't seem to follow a normal distribution
+
+# Shapiro-Wilk test (to test for normality)
+shapiro.test(residuals_arima) # p-value < 0.05, so we reject H0, residuals are not normally distributed (not good sign)
+
+# Plot residuals over time (to check for changing variance)
+plot(residuals_arima, main = "Residuals of ARIMA Model", ylab = "Residuals", xlab = "Time") # Volatility clustering, so variance is not constant over time (not good sign)
+
+"The residuals of the ARIMA model show no significant autocorrelation, which is a good sign that the model has captured 
+the data. However the residuals do not follow a normal distribution, and the variance is not constant over time. This 
+suggests that the ARIMA model may not be the best fit for the data. Maybe using a GARCH model could be more appropriate"
+
+### d
 
 
